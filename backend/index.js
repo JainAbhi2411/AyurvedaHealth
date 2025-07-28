041de3eb4a30fbe4893bae653e60825e9a65f7d5
Abhinav <jainabhi7374@gmail.com>
2025-07-28 22:20:47 +0530

    some bakcend upgrades

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,13 @@ app.use(cors({
   origin: "https://ayurvedahelp.netlify.app",
 }));
 app.use(express.json());
-
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log("Visitor IP:", ip);
+  next();
+});
 app.use("/api/chat", chatRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
